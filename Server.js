b37e2664f510d75e9db91a211ea27ef166c39e96
ruqwk99 <140777537+ruqwk99@users.noreply.github.com>
2023-09-04 22:08:45 +0900

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,7 +2,13 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+const fs = require('fs');
 
+const newData = {
+    name: 'Jane',
+    age: 25,
+    city: 'Los Angeles'
+};
 
 //.ejs 뷰엔진을 실행하기 위한 코드
 app.set('views', __dirname + '/views');
@@ -38,6 +44,30 @@ app.post('/api/data', (req, res) => {
   //const data = { message: 'Hello from Node.js server!' };
   //res.json(data);
   console.log("통신확인!");
+  fs.readFile('data.json', 'utf8', (readErr, existingData) => {
+    if (readErr) {
+        console.error('파일 읽기 중 오류가 발생했습니다.', readErr);
+    } else {
+        try {
+            // 기존 데이터 파싱
+            const parsedData = JSON.parse(existingData);
+
+            // 새로운 데이터를 배열에 추가
+            parsedData.push(newData);
+
+            // 수정된 데이터를 파일에 다시 쓰기
+            fs.writeFile('data.json', JSON.stringify(parsedData, null, 2), (writeErr) => {
+                if (writeErr) {
+                    console.error('파일 저장 중 오류가 발생했습니다.', writeErr);
+                } else {
+                    console.log('데이터가 파일에 추가되었습니다.');
+                }
+            });
+        } catch (parseError) {
+            console.error('JSON 데이터 파싱 중 오류가 발생했습니다.', parseError);
+        }
+    }
+});
   res.status(200).send("요청이 성공적으로 처리되었습니다.");
 });
 
