2be9c268aa36d9395ed53e48c63479fb05610334
ruqwk99 <140777537+ruqwk99@users.noreply.github.com>
2023-10-08 17:29:11 +0900

    Add files via upload

---
+++ TCP_Server.js
@@ -0,0 +1,39 @@
+const net = require('net');
+
+// TCP 서버 생성
+const server = net.createServer((socket) => {
+  // 클라이언트가 연결될 때 실행됩니다.
+  console.log('클라이언트가 연결되었습니다.');
+
+  // 클라이언트로부터 데이터를 받을 때 실행됩니다.
+  socket.on('data', (data) => {
+    console.log('클라이언트로부터 받은 데이터:', data.toString());
+  });
+
+  // 클라이언트 접속이 끊어질 때 실행됩니다.
+  socket.on('end', () => {
+    console.log('클라이언트 접속이 끊어졌습니다.');
+  });
+
+  // 클라이언트와의 연결이 종료될 때 실행됩니다.
+  socket.on('close', () => {
+    console.log('클라이언트와의 연결이 종료되었습니다.');
+  });
+
+  // err 예외처리
+  socket.on('error', (err) => {
+    if (err.code === 'ECONNRESET') {
+     console.log('클라이언트와의 연결이 강제로 끊어짐 -예외처리-');
+     // 연결해제
+     socket.destroy();
+    } else {
+    console.error('에러 발생:', err);
+    }
+ });
+});
+
+// 서버가 지정한 포트(예: 3000)에서 대기합니다.
+const PORT = 33333;
+server.listen(PORT, () => {
+  console.log(`서버가 ${PORT} 포트에서 대기 중...`);
+});
