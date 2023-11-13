814272d809c5834c1eb56c6a6251bc1aaff4fbe2
ruqwk99 <140777537+ruqwk99@users.noreply.github.com>
2023-11-13 23:48:05 +0900

    Update server99.js

--- server99.js
+++ server99.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 io.on('connection', (socket) => {
-  socket.on('stream', (image) => {
-    socket.broadcast.emit('stream', image);
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
   });
 });
 
 server.listen(3333, () => {
-  console.log('Server is running on port 3333*');
+  console.log('Server is running on http://localhost:3333');
 });
